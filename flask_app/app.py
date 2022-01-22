9055ff6d4ba96af0a213f4a9622f15a0f4994b3a
fibre-ether <68174050+fibre-ether@users.noreply.github.com>
2022-01-22 20:01:53 +0530

    Connect model to front-end

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template, flash, redirect, request, url_for
 from datetime import timedelta
 from markupsafe import escape
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = "password"
 
+df1 = pd.read_csv("model\\fraud_data1_1_1.csv")
+df2 = pd.read_csv("model\\fraud_data1_1_2.csv")
+df3 = pd.read_csv("model\\fraud_data2.csv")
+
+df = pd.concat([df1,df2,df3])
+
+with open('model\\fraud_model.bin', 'rb') as f_in:
+    model = pickle.load(f_in)
+
 @app.route('/', methods=["POST", "GET"])
-def index():
+def home():
     if request.method == "POST":
         if request.form["transaction_id"] == "":
             flash(" Please enter a name ", "info")
             return render_template("home.html")
-        username = request.form["transaction_id"]
-        print("aaaaa")
-        return redirect(url_for("home"))
+        id = request.form["transaction_id"]
+        row = df.loc[df['nameOrig']==int(id[1:])]
+        prediction = model.predict([list(row.iloc[0,[1,2,3,4,5,7,8]])])
+        
+        print( prediction[0] )
+        return render_template("home.html", row=row, prediction=prediction)
     else:
         return render_template("home.html")
     
