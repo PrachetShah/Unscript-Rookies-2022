43d60633871306033e2b1d9335e4bcf12ee74a27
fibre-ether <68174050+fibre-ether@users.noreply.github.com>
2022-01-22 15:06:08 +0530

    Add about page

--- flask_app/app.py
+++ flask_app/app.py
@@ -18,5 +18,9 @@ def index():
         return redirect(url_for("home"))
     else:
         return render_template("home.html")
+    
+@app.route('/about')
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug = True)
