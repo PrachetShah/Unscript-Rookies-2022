f29d03d17ab9db93a5b346e5b284f0fbfdbcf801
fibre-ether <68174050+fibre-ether@users.noreply.github.com>
2022-01-22 14:53:02 +0530

    Change homepage layout

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, url_for
 from datetime import timedelta
 from markupsafe import escape
 
@@ -9,7 +9,14 @@ app.secret_key = "password"
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template("home.html")
-
+    if request.method == "POST":
+        if request.form["transaction_id"] == "":
+            flash(" Please enter a name ", "info")
+            return render_template("home.html")
+        username = request.form["transaction_id"]
+        print("aaaaa")
+        return redirect(url_for("home"))
+    else:
+        return render_template("home.html")
 if __name__ == "__main__":
     app.run(debug = True)
